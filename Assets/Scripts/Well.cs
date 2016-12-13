f95b7301a87c4c5621b9c0a45cc1c44e58fb2357
Patrick Lathan <plathan@elon.edu>
2016-12-13 16:32:28 -0500

    readded well script with correct capitalization

---
+++ Assets/Scripts/Well.cs
@@ -0,0 +1,43 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/*
+
+WorldWall notifies players that they have hit the map boundary.
+
+Written by: Elena Sparacio and Patrick Lathan
+(C) 2016
+
+*/
+
+public class Well : MonoBehaviour {
+
+    private float progress;
+    private Vector3 startPos;
+    private float distanceToMove = 120f;
+    private Vector3 endPos;
+    private GameObject specialSeed;
+
+    // Perform calculations in local space to make it easier to see in editor
+    void Start() {
+        specialSeed = GameObject.Find("wellSeed");
+        startPos = specialSeed.transform.localPosition;
+        endPos = specialSeed.transform.localPosition + new Vector3(0, distanceToMove, 0);
+        progress = 0;
+    }
+
+    // Update is called once per frame
+    void Update() {
+
+    }
+
+    void OnParticleCollision(GameObject particle) {
+        // If a water particle collides and the specialSeed still exists, lerp it upwards
+        if (particle.tag == "waterParticle" && (specialSeed)) {
+            progress += .005f;
+            specialSeed.transform.localPosition = Vector3.Lerp(startPos, endPos, progress);
+        }
+    }
+}
+
+
