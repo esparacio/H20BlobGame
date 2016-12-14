123dfdcdd498d4481d79bafc4df311b5567f5aef
Patrick Lathan <plathan@elon.edu>
2016-12-13 23:03:08 -0500

    camera fixed time to party

--- Assets/Scripts/ThirdPersonCamera.cs
+++ Assets/Scripts/ThirdPersonCamera.cs
@@ -5,7 +5,7 @@ using System.Collections;
 
 ThirdPersonCamera is the script that deals with camera movement. 
 
-Written by: Patrick Lathan and Nathan Young
+Written by: Patrick Lathan
 (C) 2016
 
 */
@@ -15,15 +15,14 @@ public class ThirdPersonCamera : MonoBehaviour {
     private float sensitivityHor = 100.0f;
     private float sensitivityVert = 100.0f;
     // General settings - y rotation constraints
-    private float maxVert = 45f;
-    private float minVert = -45f;
+    private float maxAndMinVert = 2f;
     // General settings - initial distance from player
     private float cameraHeight = 3f;
     private float cameraDepth = 4f;
 
     // Speed that camera will return to ideal position
+    // Keep this low ideally to keep it smooth
     private float cameraZoomSpeed = 4f;
-    private float progress = 1f;
 
     // Initial camera distance from player
     private float idealDistance;
@@ -45,8 +44,8 @@ public class ThirdPersonCamera : MonoBehaviour {
         // Point at target
         transform.LookAt(player.transform);
 
+        // Set ideal distance to initial camera distance
         idealDistance = Vector3.Distance(player.transform.position, transform.position);
-
         currentDistance = idealDistance;
     }
 
@@ -57,12 +56,10 @@ public class ThirdPersonCamera : MonoBehaviour {
         // Get player input, modify by vertical sensitivity, and invert
         float xRot = Input.GetAxis("Mouse Y") * sensitivityVert * Time.deltaTime * -1;
 
-        //TODO use maxvert and minvert instead
-        //if ((transform.localPosition.z > 0 || xRot > 0) && (transform.localPosition.z < 3 || xRot < 0)) {
-        // Rotate camera around the player's x axis at the player's position
-        //Debug.Log(transform.localPosition.x + " " + transform.localPosition.y + " " + transform.localPosition.z);
+        if (!(xRot > 0 && transform.localPosition.y <= maxAndMinVert && transform.localPosition.z > 0) && !(xRot < 0 && transform.localPosition.y <= maxAndMinVert && transform.localPosition.z < 0)) {
+            // Rotate camera around the player's x axis at the player's position
             transform.RotateAround(player.transform.position, player.transform.right, xRot);
-        //}
+        }
 
         // HORIZONTAL CAMERA MOVEMENT
         // Horizontal movement rotates the actual playermodel so that powers will always be cast from the "front"
@@ -74,17 +71,13 @@ public class ThirdPersonCamera : MonoBehaviour {
         // Modify player rotation
         player.transform.localEulerAngles = new Vector3(currentXRot, yRot, 0);
 
-        //float distance = distanceFromSurface;
-
+        //adapted from http://wiki.unity3d.com/index.php?title=MouseOrbitImproved
         RaycastHit hit;
         if (Physics.Linecast(player.transform.position, (transform.position), out hit)) {
             string hitTag = hit.collider.gameObject.tag;
-            if (hitTag != "Player" && hitTag != "vapor" && hitTag != "water") {
-                //Debug.Log(hit.collider.gameObject.tag);
+            if (hitTag != "Player" && hitTag != "seed") {
                 currentDistance = hit.distance;
             }
-
-            //distance = Mathf.Clamp(distance - Input.GetAxis("Mouse ScrollWheel") * 5, distanceMin, distanceMax);
 
         } else {
             currentDistance += Time.deltaTime * cameraZoomSpeed;
@@ -93,12 +86,10 @@ public class ThirdPersonCamera : MonoBehaviour {
 
         // Vector pointing from player through camera
         Vector3 directionOfTravel = transform.position - player.transform.position;
-        // Sets magnitude of vector to original camera distance
+        // Sets magnitude of vector to current distance specified
         Vector3 finalDirection = directionOfTravel.normalized * currentDistance;
         // Adds the vector of correct length and magnitude to the current player position
         Vector3 targetPosition = player.transform.position + finalDirection;
-        //Debug.Log(targetPosition);
-        //Debug.Log(transform.position);
 
         transform.position = targetPosition;
     }
